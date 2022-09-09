dab371df1b431aec36b72816e3361c8d0f08d2f9
AlenaD-B27 <lenador.18@gmail.com>
2022-09-09 15:21:55 -0500

    Project is created, gitignore is created and task is posted.

---
+++ src/TASK.java
@@ -0,0 +1,22 @@
+public class TASK {
+    /*
+1. Accept the invitation;
+2. Clone the project to your InteliJ;
+3. Checkout to origin develop branch, it will create your local develop one;
+4. Create your branch from local develop, name of the branch should be your name. Ex: my branch will be "alena";
+5. Create the package name_lab (use your name);
+6. Create the class (custom the name, Ex: "WonderWoman", "Vampire", "MagicHamster", anything you wish in YOUR package name_lab. COMMIT
+6. Create variables: String name, String superPower1, String superPower2 and String superPower3. COMMIT
+7. Initialize variables. COMMIT
+8. Create methods useSuperPower1(), useSuperPower2(), useSuperPower3().
+
+Example: superPower1 = "eat the whole bowl of food in 2 seconds." useSuperPower1(){print: "MagicHamster Mike can eat the whole bowl of food after 2 seconds".}
+
+COMMIT
+
+8. Create toString method.  COMMIT
+9. Create new class Lab inside YOUR package and test all your methods there. COMMIT
+9. Push all the changes to GitHub.
+10. Create PR from YOUR branch to develop. If you request to pull it to master I will have to reject it, so, please, pay attention :)
+     */
+}
