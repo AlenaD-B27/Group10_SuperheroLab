ef329d6df86afcf06aeb2af9c12ede82b25883f9
AlenaD-B27 <lenador.18@gmail.com>
2022-09-09 16:03:45 -0500

    Lab.java is created and all the Cupid.java methods are tested.

---
+++ src/alena_lab/Lab.java
@@ -0,0 +1,15 @@
+package alena_lab;
+
+public class Lab {
+    public static void main(String[] args) {
+
+
+        Cupid cupid1 = new Cupid("Ben", "make people fall in love", "fly", "be forever young");
+
+        System.out.println(cupid1);
+        cupid1.useSuperpower1();
+        cupid1.useSuperpower2();
+        cupid1.useSuperpower3();
+
+    }
+}
