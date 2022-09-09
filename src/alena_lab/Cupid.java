53545e7b5e43f253e55ba0f3ec497f2cc761566d
AlenaD-B27 <lenador.18@gmail.com>
2022-09-09 15:45:44 -0500

    Cupid.java: initialized variables in Constructor

--- src/alena_lab/Cupid.java
+++ src/alena_lab/Cupid.java
@@ -5,4 +5,11 @@ public class Cupid {
     String superPower1;
     String superPower2;
     String superPower3;
+
+    public Cupid(String name, String superPower1, String superPower2, String superPower3) {
+        this.name = name;
+        this.superPower1 = superPower1;
+        this.superPower2 = superPower2;
+        this.superPower3 = superPower3;
+    }
 }
