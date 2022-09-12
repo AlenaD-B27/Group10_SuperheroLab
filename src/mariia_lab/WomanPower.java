3f65398370b3f6a1cf84c38c473e0c2a9b10e8c1
Mariia <kitmaria.2015@gmail.com>
2022-09-13 00:31:53 +0300

    created a method to instantiate the instance variable created the methods for using superPowers

--- src/mariia_lab/WomanPower.java
+++ src/mariia_lab/WomanPower.java
@@ -7,11 +7,26 @@ public class WomanPower {
     String superPower1;
     String superPower2;
     String superPower3;
-     public void setInfo(String name, String superPower1, String superPower2, String superPower3){
+    /* public void setInfo(String name, String superPower1, String superPower2, String superPower3){
          this.name = name;
          this.superPower1=superPower1;
          this.superPower2=superPower2;
          this.superPower3=superPower3;
-     }
+     }*/
+    public void setInfo(String name){
+        this.name = name;
+        this.superPower1= " can do 3 things simultaneously";
+        this.superPower2=" can coding";
+        this.superPower3=" creates magic";
+    }
+    public final String useSuperPower1(){
+        return name + " like a Caesar " + superPower1;
+    }
+    public final String useSuperPower2(){
+        return name + " " + superPower2 + " all the night";
+    }
+    public final String useSuperPower3(){
+        return name + " " + superPower3 +" with HTML+CSS+JavaScript ";
+    }
 
 }
