fb49dfb505f77d2f78c2df6371b68a7e65466b3e
Mariia <kitmaria.2015@gmail.com>
2022-09-12 23:48:44 +0300

    instantiated the instance variable

--- src/mariia_lab/WomanPower.java
+++ src/mariia_lab/WomanPower.java
@@ -1,8 +1,17 @@
 package mariia_lab;
 
+import java.security.SecureRandom;
+
 public class WomanPower {
     String name;
     String superPower1;
     String superPower2;
     String superPower3;
+     public void setInfo(String name, String superPower1, String superPower2, String superPower3){
+         this.name = name;
+         this.superPower1=superPower1;
+         this.superPower2=superPower2;
+         this.superPower3=superPower3;
+     }
+
 }
