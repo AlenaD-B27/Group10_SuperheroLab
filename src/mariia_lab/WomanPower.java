4421ded7739f482f7fdc443e0ef202abe2635352
Mariia <kitmaria.2015@gmail.com>
2022-09-13 21:20:43 +0300

    created constructor changes the methods to void edit the toString();

--- src/mariia_lab/WomanPower.java
+++ src/mariia_lab/WomanPower.java
@@ -13,25 +13,24 @@ public class WomanPower {
          this.superPower2=superPower2;
          this.superPower3=superPower3;
      }*/
-    public void setInfo(String name){
+    public WomanPower(String name){
         this.name = name;
-        this.superPower1= " can do 3 things simultaneously";
-        this.superPower2=" can coding";
-        this.superPower3=" creates magic";
+        superPower1= " can do 3 things simultaneously";
+        superPower2=" can coding";
+        superPower3=" creates magic";
     }
-    public final String useSuperPower1(){
-        return name + " like a Caesar " + superPower1;
+    public void useSuperPower1(){
+        System.out.println(name + " like a Caesar " + superPower1);
     }
-    public final String useSuperPower2(){
-        return name + " " + superPower2 + " all the night";
+    public void useSuperPower2(){
+        System.out.println(name + " " + superPower2 + " all the night");
     }
-    public final String useSuperPower3(){
-        return name + " " + superPower3 +" with HTML+CSS+JavaScript ";
+    public void useSuperPower3(){
+        System.out.println(name + " " + superPower3 +" with HTML+CSS+JavaScript ");
     }
     @Override
     public String toString() {
         return  name + " has these super powers: " +"\n1)"+
-                superPower1 + "\n2)" +superPower2 +"\n3)"+ superPower3 +
-                "\n" + useSuperPower1() + "\n" +useSuperPower2() + "\n" + useSuperPower3();
+                superPower1 + "\n2)" +superPower2 +"\n3)"+ superPower3;
     }
 }
