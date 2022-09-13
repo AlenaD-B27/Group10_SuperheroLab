262db492ca51d09b20aecb75b8be87b5ed482670
olhasoroka <olly0296@gmail.com>
2022-09-13 12:43:36 -0500

    MiracleCat: initialized variables in constructor

--- src/olga_lab/MiracleCat.java
+++ src/olga_lab/MiracleCat.java
@@ -7,4 +7,10 @@ public class MiracleCat {
     String superPower2;
     String superPower3;
 
+    public MiracleCat(String name, String superPower1, String superPower2, String superPower3) {
+        this.name = name;
+        this.superPower1 = superPower1;
+        this.superPower2 = superPower2;
+        this.superPower3 = superPower3;
+    }
 }
