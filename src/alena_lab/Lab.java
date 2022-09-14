9358b2e00e03303ed06fdf888dce8bd39c3eec5d
AlenaD-B27 <lenador.18@gmail.com>
2022-09-14 12:13:33 -0500

    added obj cupid2 for second example

--- src/alena_lab/Lab.java
+++ src/alena_lab/Lab.java
@@ -6,10 +6,16 @@ public class Lab {
 
         Cupid cupid1 = new Cupid("Ben", "make people fall in love", "fly", "be forever young");
 
+        Cupid cupid2 = new Cupid("Paul", "make people hate each other", "fly", "be forever old and grumpy");
+
         System.out.println(cupid1);
         cupid1.useSuperpower1();
         cupid1.useSuperpower2();
         cupid1.useSuperpower3();
+        System.out.println("===================");
+        cupid2.useSuperpower1();
+        cupid2.useSuperpower2();
+        cupid2.useSuperpower3();
 
     }
 }
